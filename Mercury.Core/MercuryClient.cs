b9965b1e806449683e1b694d2b40fe320ec42dd6
ShadowWorx <mdbarker813@att.net>
2026-07-04 14:20:11 -0400

    Add initial Mercury.Core project code

---
+++ Mercury.Core/MercuryClient.cs
@@ -0,0 +1,52 @@
+﻿// ***********************************************************************
+// Assembly       : Mercury.Core
+// Author           : Matthew D. Barker
+// Created          : 07-02-2026
+//
+// Last Modified By : Matthew D. Barker
+// Last Modified On : 07-04-2026
+// ***********************************************************************
+// <copyright file="MercuryClient.cs">
+//     Copyright (c) Matthew D. Barker. All rights reserved.
+//     Submitted in partial fulfillment of CSE499 Senior Capstone Project
+//     at Brigham Young University-Idaho.
+// </copyright>
+// ***********************************************************************
+
+using Mercury.Abstractions;
+using Mercury.Abstractions.Cryptography;
+using Mercury.Abstractions.Primitives;
+
+namespace Mercury.Core;
+
+/// <summary>
+/// Class MercuryClient.
+/// Implements the <see cref="IMercuryClient" />
+/// </summary>
+/// <seealso cref="IMercuryClient" />
+internal class MercuryClient : IMercuryClient
+{
+    /// <summary>
+    /// Sends the asynchronous.
+    /// </summary>
+    /// <param name="cryptoContext">The crypto context.</param>
+    /// <param name="payload">The payload.</param>
+    /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <returns>Task.</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task SendAsync(ICryptoContext cryptoContext, ReadOnlyMemory payload, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Receives the asynchronous.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <returns>Task&lt;IMercuryResult&gt;.</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Task<IMercuryResult> ReceiveAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
