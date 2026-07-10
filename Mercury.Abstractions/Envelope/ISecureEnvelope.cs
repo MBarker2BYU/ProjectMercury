dc9a2e5321b567d470a18654dbbe5d71c8564d5c
ShadowWorx <mdbarker813@att.net>
2026-07-10 01:10:55 -0400

    SecureEnvelope base created.

---
+++ Mercury.Abstractions/Envelope/ISecureEnvelope.cs
@@ -0,0 +1,30 @@
+﻿// ***********************************************************************
+// Assembly       : Mercury.Abstractions
+// Author           : Matthew D. Barker
+// Created          : 07-02-2026
+//
+// Last Modified By : Matthew D. Barker
+// Last Modified On : 07-09-2026
+// ***********************************************************************
+// <copyright file="ISecureEnvelope.cs">
+//     Copyright (c) Matthew D. Barker. All rights reserved.
+//     Submitted in partial fulfillment of CSE499 Senior Capstone Project
+//     at Brigham Young University-Idaho.
+// </copyright>
+// ***********************************************************************
+
+using Mercury.Abstractions.Primitives;
+
+namespace Mercury.Abstractions.Envelope;
+
+/// <summary>
+/// Interface ISecureEnvelope
+/// </summary>
+public interface ISecureEnvelope
+{
+    /// <summary>
+    /// Gets the payload.
+    /// </summary>
+    /// <value>The payload.</value>
+    ReadOnlyMemory Payload { get; }
+}
---
+++ Mercury.Core/Envelope/SecureEnvelope.cs
@@ -0,0 +1,36 @@
+﻿// ***********************************************************************
+// Assembly       : Mercury.Core
+// Author           : Matthew D. Barker
+// Created          : 07-02-2026
+//
+// Last Modified By : Matthew D. Barker
+// Last Modified On : 07-09-2026
+// ***********************************************************************
+// <copyright file="SecureEnvelope.cs">
+//     Copyright (c) Matthew D. Barker. All rights reserved.
+//     Submitted in partial fulfillment of CSE499 Senior Capstone Project
+//     at Brigham Young University-Idaho.
+// </copyright>
+// ***********************************************************************
+
+using Mercury.Abstractions.Envelope;
+using Mercury.Abstractions.Primitives;
+
+namespace Mercury.Core.Envelope;
+
+/// <summary>
+/// Class SecureEnvelope. This class cannot be inherited.
+/// Implements the <see cref="ISecureEnvelope" />
+/// </summary>
+/// <param name="payload">The payload.</param>
+/// <seealso cref="ISecureEnvelope" />
+internal sealed class SecureEnvelope(ReadOnlyMemory payload) : ISecureEnvelope
+{
+    /// <summary>
+    /// Gets the payload.
+    /// </summary>
+    /// <value>The payload.</value>
+    public ReadOnlyMemory Payload { get; } = !payload.IsEmpty 
+        ? payload 
+        : throw new ArgumentException("The secure envelope cannot be created with an empty payload.", nameof(payload));
+}
