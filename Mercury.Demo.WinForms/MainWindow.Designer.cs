9a4744d7899f4cb821588d09a1820b2b7a6544ec
ShadowWorx <mdbarker813@att.net>
2026-07-12 16:26:28 -0400

    - Demo cleared

--- Mercury.Demo.WinForms/MainWindow.Designer.cs
+++ Mercury.Demo.WinForms/MainWindow.Designer.cs
@@ -28,58 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnLoopback = new Button();
-            btnAlphaToBravo = new Button();
-            btnBravoToAlpha = new Button();
             SuspendLayout();
-            // 
-            // btnLoopback
-            // 
-            btnLoopback.Location = new Point(28, 69);
-            btnLoopback.Name = "btnLoopback";
-            btnLoopback.Size = new Size(75, 23);
-            btnLoopback.TabIndex = 0;
-            btnLoopback.Text = "Loopback";
-            btnLoopback.UseVisualStyleBackColor = true;
-            btnLoopback.Click += button1_Click;
-            // 
-            // btnAlphaToBravo
-            // 
-            btnAlphaToBravo.Location = new Point(454, 407);
-            btnAlphaToBravo.Name = "btnAlphaToBravo";
-            btnAlphaToBravo.Size = new Size(75, 23);
-            btnAlphaToBravo.TabIndex = 1;
-            btnAlphaToBravo.Text = "ToBravo";
-            btnAlphaToBravo.UseVisualStyleBackColor = true;
-            btnAlphaToBravo.Click += btnAlphaToBravo_Click;
-            // 
-            // btnBravoToAlpha
-            // 
-            btnBravoToAlpha.Location = new Point(646, 407);
-            btnBravoToAlpha.Name = "btnBravoToAlpha";
-            btnBravoToAlpha.Size = new Size(75, 23);
-            btnBravoToAlpha.TabIndex = 2;
-            btnBravoToAlpha.Text = "ToAlpha";
-            btnBravoToAlpha.UseVisualStyleBackColor = true;
-            btnBravoToAlpha.Click += btnBravoToAlpha_Click;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(883, 595);
-            Controls.Add(btnBravoToAlpha);
-            Controls.Add(btnAlphaToBravo);
-            Controls.Add(btnLoopback);
             Name = "MainWindow";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button btnLoopback;
-        private Button btnAlphaToBravo;
-        private Button btnBravoToAlpha;
     }
 }
--- Mercury.Demo.WinForms/MainWindow.cs
+++ Mercury.Demo.WinForms/MainWindow.cs
@@ -1,14 +1,3 @@
-using Mercury.Abstractions;
-using Mercury.Abstractions.Enums;
-using Mercury.Abstractions.Primitives;
-using Mercury.Core.Factories;
-using Mercury.Provider.AesGcm;
-using Mercury.Transport.InMemory;
-using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Text;
-using Mercury.Abstractions.Shared;
-
 namespace Mercury.Demo.WinForms
 {
 
@@ -19,95 +8,14 @@ namespace Mercury.Demo.WinForms
     /// <seealso cref="System.Windows.Forms.Form" />
     public partial class MainWindow : Form
     {
-        internal const string ALPHA_NODE = "Alpha Node";
-        internal const string BRAVO_NODE = "Bravo Node";
-
+       
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-
-            InitTransport();
         }
-
-        private void InitTransport()
-        {
-            var (alphaTransport, bravoTransport) = InMemoryDuplexTransport.CreateConnectedPair();
-
-            var keys = new Dictionary<KeyId, byte[]>
-            {
-                [ALPHA_NODE] = RandomNumberGenerator.GetBytes(32),
-                [BRAVO_NODE] = RandomNumberGenerator.GetBytes(32)
-            };
-
-            var keyProvider = new SymmetricKeyProviderDictionary(keys);
-            
-            var alphaAesGcm = new AesGcmCryptoProvider(keyProvider);
-            var bravoAesGcm = new AesGcmCryptoProvider(keyProvider);
-
-            var alphaDependencies = MercuryFactory.Instance.BuildDependencies(alphaAesGcm, EnvelopeCodec.Json, alphaTransport);
-            var bravoDependencies = MercuryFactory.Instance.BuildDependencies(bravoAesGcm, EnvelopeCodec.Json, bravoTransport);
-
-            m_AlphaClient = MercuryFactory.Instance.BuildClient(alphaDependencies);
-            m_BravoClient = MercuryFactory.Instance.BuildClient(bravoDependencies);
-
-        }
-
-        private IMercuryClient m_AlphaClient = null!;
-        private IMercuryClient m_BravoClient = null!;
-
-
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            var result = TestClient();
-
-            if (!result.Result.Success)
-                throw new Exception(result.Result.Message);
-
-            var value = result.Result.Payload.ToArray()[2];
-
-            Debug.WriteLine($@"{value}");
-        }
-
-        private static async Task<IMercuryResult> TestClient()
-        {
-            var mercuryClient = MercuryFactory.Instance.BuildClient();
-            var cryptoContext = MercuryFactory.Instance.BuildCryptoContext(ALPHA_NODE, BRAVO_NODE);
-
-            await mercuryClient.SendAsync(cryptoContext, new ReadOnlyMemory([1, 2, 3]));
-
-            var result = mercuryClient.ReceiveAsync();
-
-            return await result;
-        }
-
-        private async void btnAlphaToBravo_Click(object sender, EventArgs e)
-        {
-            const string message = @"Alpha One to Bravo Actual";
-
-            var cryptoContext = MercuryFactory.Instance.BuildCryptoContext(ALPHA_NODE, BRAVO_NODE);
-
-            await m_AlphaClient.SendAsync(cryptoContext,Encoding.UTF8.GetBytes(message));
-
-            var mercuryResult = await m_BravoClient.ReceiveAsync();
-
-            Debug.WriteLine($"{Encoding.UTF8.GetString(mercuryResult.Payload.ToArray())}");
-
-        }
-
-        private async void btnBravoToAlpha_Click(object sender, EventArgs e)
-        {
-            const string message = @"Bravo Actual to Alpha One";
-
-            var cryptoContext = MercuryFactory.Instance.BuildCryptoContext( BRAVO_NODE, ALPHA_NODE);
-
-            await m_BravoClient.SendAsync(cryptoContext, Encoding.UTF8.GetBytes(message));
-
-            var mercuryResult = await m_AlphaClient.ReceiveAsync();
-
-            Debug.WriteLine($"{Encoding.UTF8.GetString(mercuryResult.Payload.ToArray())}");
-        }
+        
     }
 }
